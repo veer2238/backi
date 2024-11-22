3a01638db65ac3248d5a1f005e862617dc8312ff
nanu2705 <nandanipatel057@gmail.com>
2024-11-22 10:59:16 +0530

    back

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import detailroute from "./routes/detailroute.js"
+import datas from "./routes/Api/data.js"
 
+dotenv.config();
+const app = express();
 
-const app = express()
+app.use(cors());
+app.use(express.json());
 
-app.get('/api', (req, res) => {
-    res.send('Hello World!')
-    })
+const DB = process.env.MONGODB_URL
+mongoose
+  .connect(DB)
+  .then(() => console.log("mongodb connected"))
+  .catch((err) => console.log("mongo error", err));
+
+app.use('/', datas)
+app.use('/', detailroute)
+
 
 app.listen(3035, () => {
     console.log('Server connected');
