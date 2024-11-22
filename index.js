c2d5f70c9402a0af5a12e8f4a51219bbf886b4a5
nanu2705 <nandanipatel057@gmail.com>
2024-11-22 11:31:01 +0530

    back

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import detailroute from "./routes/detailroute.js"
 import contactroute from "./routes/contactroute.js"
+import Inquiryroute from "./routes/Inquiryroute.js"
+import newsletterroute from "./routes/newsletterroute.js"
+import razorpayroute from "./routes/razorpayroute.js"
 import datas from "./routes/Api/data.js"
 import faq from "./routes/Api/faq.js"
 import reviews from "./routes/Api/reviews.js"
@@ -25,6 +28,10 @@ app.use('/',faq)
 app.use('/', reviews)
 app.use('/', contactroute)
 app.use('/', detailroute)
+app.use('/', Inquiryroute)
+app.use('/', newsletterroute)
+app.use('/', razorpayroute)
+
 
 
 app.listen(3035, () => {
