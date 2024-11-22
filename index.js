63c7ee347d4fbbfe2300a98806c355b1d1c85065
nanu2705 <nandanipatel057@gmail.com>
2024-11-22 17:17:09 +0530

    back

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ import detailroute from "./routes/detailroute.js"
 import contactroute from "./routes/contactroute.js"
 import Inquiryroute from "./routes/Inquiryroute.js"
 import newsletterroute from  "./routes/newsletterroute.js"
-import razorpayroute from "./routes/razorpayroute.js"
 import datas from "./routes/Api/data.js"
 import faq from "./routes/Api/faq.js"
 import reviews from "./routes/Api/reviews.js"
@@ -38,7 +37,6 @@ app.use('/', contactroute)
 app.use('/', detailroute)
 app.use('/', Inquiryroute)
 app.use('/', newsletterroute)
-app.use('/', razorpayroute)
 
 
 
